194d28af6ae1ddce34a6fa5b4ae12d6a2e6651db
Rodrigo Vieira <rodrigodenizads@gmail.com>
2021-07-05 22:52:18 -0300

    refactor: ensure erros eslint

--- jest.config.js
+++ jest.config.js
@@ -1,18 +1,18 @@
 module.exports = {
-  roots: ["<rootDir>/src"],
+  roots: ['<rootDir>/src'],
   collectCoverageFrom: [
-    "<rootDir>/src/**/*.{ts,tsx}", "!**/*.d.ts",
-    "!<rootDir>/src/main/**/*",
-    "!<rootDir>/src/presentation/components/router/**/*",
-    "!**/*.d.ts"
+    '<rootDir>/src/**/*.{ts,tsx}', '!**/*.d.ts',
+    '!<rootDir>/src/main/**/*',
+    '!<rootDir>/src/presentation/components/router/**/*',
+    '!**/*.d.ts'
   ],
-  coverageDirectory: "coverage",
-  testEnvironment: "jsdom",
+  coverageDirectory: 'coverage',
+  testEnvironment: 'jsdom',
   transform: {
-    ".+\\.(ts|tsx)$": "ts-jest",
+    '.+\\.(ts|tsx)$': 'ts-jest'
   },
   moduleNameMapper: {
-    "@/(.*)": "<rootDir>/src/$1",
-    "\\.scss$": "identity-obj-proxy",
-  },
+    '@/(.*)': '<rootDir>/src/$1',
+    '\\.scss$': 'identity-obj-proxy'
+  }
 }
