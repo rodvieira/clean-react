ee3ad6646a891faf6698e05a09775f9a81fe048e
Rodrigo Vieira <rodrigodenizads@gmail.com>
2021-11-08 20:08:36 -0300

    refactor: improve input layout

--- src/presentation/components/input/input.tsx
+++ src/presentation/components/input/input.tsx
@@ -12,10 +12,15 @@ const Input: React.FC<Props> = (props: Props) => {
   const inputRef = useRef<HTMLInputElement>()
   const error = state[`${props.name}Error`]
   return (
-    <div className={Styles.inputWrap}>
+    <div
+      data-testid={`${props.name}-wrap`}
+      className={Styles.inputWrap}
+      data-status={error ? 'invalid' : 'valid'}
+    >
       <input
         {...props}
         ref={inputRef}
+        title={error}
         placeholder=" "
         data-testid={props.name}
         readOnly
@@ -23,13 +28,13 @@ const Input: React.FC<Props> = (props: Props) => {
         onChange={e => setState({ ...state, [e.target.name]: e.target.value })
       }
       />
-      <label onClick={() => inputRef.current.focus()}>{props.placeholder}</label>
-      <span
-        data-testid={`${props.name}-status`}
-        title={error || 'Tudo certo!'}
-        className={Styles.status}>
-        {error ? '🔴' : '🟢'}
-      </span>
+      <label
+        data-testid={`${props.name}-label`}
+        title={error}
+        onClick={() => inputRef.current.focus()}
+      >
+        {props.placeholder}
+      </label>
     </div>
   )
 }
