6ad6531f78ceb9cfd777d5c16f16df852d0bdbd3
Rodrigo Vieira <rodrigodenizads@gmail.com>
2021-11-18 23:16:21 -0300

    refactor: remove generic body type of http post client

--- src/data/usecases/add-account/remote-add-account.ts
+++ src/data/usecases/add-account/remote-add-account.ts
@@ -6,7 +6,7 @@ import { EmailInUserError, UnexpectedError } from '@/domain/errors'
 export class RemoteAddAccount implements AddAccount {
   constructor (
     private readonly url: string,
-    private readonly httpPostClient: HttpPostClient<AddAccountParams, AccountModel>
+    private readonly httpPostClient: HttpPostClient<AccountModel>
   ) {}
 
   async add (params: AddAccountParams): Promise<AccountModel> {
--- src/data/usecases/authentication/remote-authentication.spec.ts
+++ src/data/usecases/authentication/remote-authentication.spec.ts
@@ -3,20 +3,16 @@ import { HttpPostClientSpy } from '@/data/test'
 import { HttpStatusCode } from '@/data/protocols/http'
 import { mockAccountModel, mockAuthentication } from '@/domain/test'
 import { InvalidCredentialsError, UnexpectedError } from '@/domain/errors'
-import { AuthenticationParams } from '@/domain/usecases/authentication'
 import { AccountModel } from '@/domain/models'
 import faker from 'faker'
 
 type SutTypes = {
   sut: RemoteAuthentication
-  httpPostClientSpy: HttpPostClientSpy<AuthenticationParams, AccountModel>
+  httpPostClientSpy: HttpPostClientSpy<AccountModel>
 }
 
 const makeSut = (url: string = faker.internet.url()): SutTypes => {
-  const httpPostClientSpy = new HttpPostClientSpy<
-  AuthenticationParams,
-  AccountModel
-  >()
+  const httpPostClientSpy = new HttpPostClientSpy<AccountModel >()
   const sut = new RemoteAuthentication(url, httpPostClientSpy)
 
   return { sut, httpPostClientSpy }
