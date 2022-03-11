9e9427d65fb660edea099f0edbbc8012b0e503bf
Rodrigo Vieira <rodrigodenizads@gmail.com>
2022-03-10 23:40:18 -0300

    refactor: move survey list items to components

---
+++ src/presentation/pages/survey-list/components/survey-item-empty/survey-item-empty.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+import Styles from './survey-item-empty.scss'
+
+const SurveyItemEmpty: React.FC = () => {
+  return (
+    <>
+      <li className={Styles.surveyItemEmpty}></li>
+      <li className={Styles.surveyItemEmpty}></li>
+      <li className={Styles.surveyItemEmpty}></li>
+      <li className={Styles.surveyItemEmpty}></li>
+      <li className={Styles.surveyItemEmpty}></li>
+    </>
+  )
+}
+
+export default SurveyItemEmpty
