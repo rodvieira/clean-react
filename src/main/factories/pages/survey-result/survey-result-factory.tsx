17e7ab190792b541bfc4e37ea7d24c78410dda9a
Rodrigo Vieira <rodrigodenizads@gmail.com>
2022-05-31 11:37:25 -0300

    feat: add SurveyResult composition

---
+++ src/main/factories/pages/survey-result/survey-result-factory.tsx
@@ -0,0 +1,17 @@
+import { makeRemoteLoadSurveyResult } from '@/main/factories/usecases'
+import { SurveyResult } from '@/presentation/pages'
+import { useParams } from 'react-router-dom'
+import React from 'react'
+
+type Params = {
+  id: string
+}
+
+export const makeSurveyResult: React.FC = () => {
+  const { id } = useParams<Params>()
+  return (
+    <SurveyResult
+      loadSurveyResult={makeRemoteLoadSurveyResult(id)}
+    />
+  )
+}
