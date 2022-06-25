1b51634ce17684f1b22214417d9da876064b7c02
Rodrigo Vieira <rodrigodenizads@gmail.com>
2022-06-25 13:48:05 -0300

    fix: change test to prevent multiple clicks

--- src/presentation/test/render-helper.tsx
+++ src/presentation/test/render-helper.tsx
@@ -4,27 +4,31 @@ import { mockAccountModel } from '@/domain/test'
 import { render } from '@testing-library/react'
 import { Router } from 'react-router-dom'
 import { MemoryHistory } from 'history'
-import { RecoilRoot } from 'recoil'
+import { MutableSnapshot, RecoilRoot, RecoilState } from 'recoil'
 import React from 'react'
 
 type Params = {
   Page: React.FC
   history: MemoryHistory
   account?: AccountModel
+  states?: Array<{ atom: RecoilState<any>, value: any}>
 }
 
 type Result = {
   setCurrentAccountMock: (account: AccountModel) => void
 }
 
-export const renderWithHistory = ({ Page, history, account = mockAccountModel() }: Params): Result => {
+export const renderWithHistory = ({ Page, history, account = mockAccountModel(), states = [] }: Params): Result => {
   const setCurrentAccountMock = jest.fn()
   const mockedState = {
     setCurrentAccount: setCurrentAccountMock,
     getCurrentAccount: () => account
   }
+  const initializeState = ({ set }: MutableSnapshot): void => {
+    [...states, { atom: currentAccountState, value: mockedState }].forEach(state => set(state.atom, state.value))
+  }
   render(
-    <RecoilRoot initializeState={({ set }) => set(currentAccountState, mockedState)}>
+    <RecoilRoot initializeState={initializeState}>
       <Router history={history}>
         <Page />
       </Router>
